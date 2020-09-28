e7d50414ef9daf03702b3d98d261ab4179f478db
Marcos Velasco <devslife7@gmail.com>
2020-09-27 23:37:12 -0400

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :user_projects
   has_many :projects, :through => :user_projects
   has_many :user_tasks
   has_many :tasks, :through => :user_tasks
+
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
