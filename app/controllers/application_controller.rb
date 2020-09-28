eb4e26c78267cc25b7a9cbcde8762ff17955c628
Marcos Velasco <devslife7@gmail.com>
2020-09-27 23:29:12 -0400

    adding user authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+
+  def encode_token(payload)
+    JWT.encode(payload, "Secret", "HS256")
+  end
+
+  def logged_in?
+    headers = request.headers["Authorization"]
+    token = request.split("")[1]
+
+    begin
+      user_id = JWT.decode(token, "Secret", "HS256")[0]["user_id"]
+      User.find(user_id)
+    rescue
+      nil
+    end
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  def signup
+    byebug
+    user = User.new(user_params)
+
+    if user.valid?
+      user.save
+      token = encode_token({ user_id: user.id })
+
+      render json: { user: user, token: token },
+        except: [:created_at, :updated_at, :password_digest],
+        include: [
+          :invitations,
+          :watchparties,
+          :friends => {
+            except: [:created_at, :updated_at, :password_digest]
+          },
+          :user_leagues => {
+            only: [:id],
+            include: [
+              :league => { except: [:created_at, :updated_at] }
+            ]
+          }]
+    else
+      render json: { error: user.errors }
+    end
+  end
+
 end
