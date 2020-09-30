4a5bdff9c1f2f72c8fcae641865808fb47452da2
Marcos Velasco <devslife7@gmail.com>
2020-09-30 11:36:42 -0400

    added projects index action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,9 @@
 class ProjectsController < ApplicationController
+  skip_before_action :authorized, only: [:index]
+
+  def index
+    projects = Project.all
+
+    render json: projects, include: [:tasks]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :user_tasks
   # resources :tasks
   # resources :user_projects
-  # resources :projects
+  resources :projects, only: [:index]
   resources :users, only: [:create, :show]
   post '/login', to: 'auth#login'
 
