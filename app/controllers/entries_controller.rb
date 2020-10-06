b590781828e92ae7fec0e7e6a48cc4761b93fadd
Marcos Velasco <devslife7@gmail.com>
2020-10-06 00:45:15 -0400

    updates task on entry creation

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,28 +2,12 @@ class EntriesController < ApplicationController
   skip_before_action :authorized, only: [:create, :destroy]
 
   def create
-    task = Task.find_by(id: params[:entry][:task_id])
     entry = Entry.create(entry_params)
-    
-    sum = 0
-    task.entries.each do |entry|
-      sum = sum + entry.completion_percentage.to_i
-    end
-    # task.update({completion_percentage: `#{sum}%`})
-    str = "#{sum}%"
-    task.update({completion_percentage: str})
+
+    entry.update_tree
 
 
-    project = task.project
-    sum1 = 0
-    project.tasks.each do |task|
-      sum1 = sum1 + task.completion_percentage.to_i
-    end
-    avg = (sum1/project.tasks.length).ceil()
-
-    byebug
-
-    project.update({completion_percentage: "#{avg}%"})
+    # byebug
 
 
     if entry
@@ -47,6 +31,6 @@ class EntriesController < ApplicationController
   private
 
   def entry_params
-    params.require(:entry).permit(:date, :notes, :completion_percentage, :user_id, :task_id)
+    params.require(:entry).permit(:date, :progress, :notes, :task_id)
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,33 @@ class Entry < ApplicationRecord
   belongs_to :task
   has_many :user_entries
   has_many :users, :through => :user_entries
+
+  def update_tree
+
+    # udpate task progress
+    task = self.task
+    task.update(progress: self.progress)
+
+    # update milestones progress
+    milestone = task.milestone
+
+    # milestone.tasks.map do |task|
+      
+    # end
+
+    # update project progress
+    project = milestone.project
+
+    
+    # project = task.project
+    # sum1 = 0
+    # project.tasks.each do |task|
+    #   sum1 = sum1 + task.completion_percentage.to_i
+    # end
+    # avg = (sum1/project.tasks.length).ceil()
+
+    # project.update({completion_percentage: "#{avg}%"})
+    byebug
+    "updating #{self.name}"
+  end
 end
