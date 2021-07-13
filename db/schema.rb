f9d71cd157bb5c945492b6e3cdbb51b5438ff9b0
Marcos Velasco <devslife7@gmail.com>
2021-07-12 21:25:29 -0400

    added email to users database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_230749) do
+ActiveRecord::Schema.define(version: 2021_07_13_012240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_10_01_230749) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "entries", "tasks"
