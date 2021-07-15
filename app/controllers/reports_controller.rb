c85324d94cc4a82af709974f3d75ae38072064ab
Marcos Velasco <devslife7@gmail.com>
2021-07-14 20:19:43 -0400

    added title to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,30 @@
 class ReportsController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def create
+
+    user = User.find_by(id: report_params[:user_id])
+    project = Project.find_by(id: report_params[:project_id])
+    
+    byebug
+    if user && project
+      report = Report.new(report_params)
+      if report
+        report.save
+        render json: { report: report }
+      else
+        render json: { error: 'Report could not be saved'}
+      end
+    else
+      render json: { error: 'User or Report could not be found'}
+    end
+
+  
+  end
+
+  private
+  
+  def report_params
+    params.require(:report).permit(:user_id, :project_id, :notes)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reports
+  resources :reports, only: [:create]
   resources :entries, only: [:create, :update, :destroy]
   resources :milestones, only: [:show]
   # resources :user_tasks
