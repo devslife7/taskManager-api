c3a445e1fdc7fa00dae62e3f1c06454495751a6b
Marcos Velasco <devslife7@gmail.com>
2021-07-24 13:19:51 -0400

    fixed deleting tasks error

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,17 +5,11 @@ class Task < ApplicationRecord
   has_many :entries, :dependent => :destroy
 
   def update_progress
-    if !self.entries.empty?
-      last_entry_progress = self.entries.last.progress
-    else 
-      last_entry_progress = 0 # when entry array is empty the progress is set to 0
-    end
+    last_entry_progress = self.entries.last.progress
     self.update(progress: last_entry_progress)
   end
 
   def update_progress_tree
-    # udpate task progress
-    self.update_progress
 
     # update milestone progress
     milestone = self.milestone
